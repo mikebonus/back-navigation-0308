e321319f9b92b7d9d16ee20bc6db99a33b8310b4
MIKE <mikebonus777@gmail.com>
2023-03-08 15:31:34 -0500

    1.Forgot Password[Text-Realignment] 2.Sign-up/Sign-in Screen Piling Issue 3.Back-press button in Log-in screen

--- app/src/main/java/com/luxpmsoft/luxaipoc/view/activity/registration/SignUpActivity.kt
+++ app/src/main/java/com/luxpmsoft/luxaipoc/view/activity/registration/SignUpActivity.kt
@@ -29,6 +29,7 @@ import com.luxpmsoft.luxaipoc.widget.FacebookLogin
 import com.luxpmsoft.luxaipoc.widget.SignInGoogle
 import kotlinx.android.synthetic.main.activity_sign_in1.*
 
+// 2023-03-08
 class SignUpActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySignUpBinding
